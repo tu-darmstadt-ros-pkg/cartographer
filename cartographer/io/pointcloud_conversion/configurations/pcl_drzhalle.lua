45e1c7156f9ec08bc1bcd862805f1e75d386ec04
leo <leo.hampel@online.de>
2021-07-06 00:59:40 +0200

    ProtoBuffers: Solved problem with creation of submap

--- cartographer/io/pointcloud_conversion/configurations/pcl_drzhalle.lua
+++ cartographer/io/pointcloud_conversion/configurations/pcl_drzhalle.lua
@@ -3,22 +3,23 @@
 options = {
     generateCubicPointcloud = false,
 
-    pointcloudPath = "/Downloads/Halle-DRZ-Modell-innen-Flug1-2020-12-09.ply",
+    --pointcloudPath = "/Downloads/Halle-DRZ-Modell-innen-Flug1-2020-12-09.ply",
+    pointcloudPath = "/Downloads/Halle-DRZ-Modell-innen-Flug1-2020-12-09-part-part.ply",
 
     uniformDownSample = true,
     sampleRateUniformDownSample = 10,
 
     voxelDownSample = true,
-    voxelSizeVoxelDownSample = 0.1,
+    voxelSizeVoxelDownSample = 0.03,
 
     removeRadiusOutliers = true,
     sphereSizeRadiusOutliers = 0.3,
-    neighborsInSphereRadiusOutlier = 2,
+    neighborsInSphereRadiusOutlier = 100,
 
     cutRoofZAxis = false,
     cutoffSize = 3.0,
 
-    normalOrientationNearestNeighbours = 40,
+    normalOrientationNearestNeighbours = 100,
 
     absoluteVoxelSize = 0.1,
     absoluteTruncationDistance = 0.3,
