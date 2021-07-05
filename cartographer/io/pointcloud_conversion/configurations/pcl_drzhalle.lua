83d507e7dc7a6ad7e23949e39ac649c3b2bf05d9
leo <leo.hampel@online.de>
2021-07-06 01:13:18 +0200

    Revert "ProtoBuffers: Solved problem with creation of submap"
    This reverts commit 45e1c7156f9ec08bc1bcd862805f1e75d386ec04.

--- cartographer/io/pointcloud_conversion/configurations/pcl_drzhalle.lua
+++ cartographer/io/pointcloud_conversion/configurations/pcl_drzhalle.lua
@@ -3,23 +3,22 @@
 options = {
     generateCubicPointcloud = false,
 
-    --pointcloudPath = "/Downloads/Halle-DRZ-Modell-innen-Flug1-2020-12-09.ply",
-    pointcloudPath = "/Downloads/Halle-DRZ-Modell-innen-Flug1-2020-12-09-part-part.ply",
+    pointcloudPath = "/Downloads/Halle-DRZ-Modell-innen-Flug1-2020-12-09.ply",
 
     uniformDownSample = true,
     sampleRateUniformDownSample = 10,
 
     voxelDownSample = true,
-    voxelSizeVoxelDownSample = 0.03,
+    voxelSizeVoxelDownSample = 0.1,
 
     removeRadiusOutliers = true,
     sphereSizeRadiusOutliers = 0.3,
-    neighborsInSphereRadiusOutlier = 100,
+    neighborsInSphereRadiusOutlier = 2,
 
     cutRoofZAxis = false,
     cutoffSize = 3.0,
 
-    normalOrientationNearestNeighbours = 100,
+    normalOrientationNearestNeighbours = 40,
 
     absoluteVoxelSize = 0.1,
     absoluteTruncationDistance = 0.3,
